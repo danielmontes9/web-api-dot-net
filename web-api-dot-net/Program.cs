70f7ea793e35bbe405b4670e4f3c7972ce3f3952
Daniel Montes <gdanielmontesh9@gmail.com>
2025-04-18 17:49:57 -0600

    refactor: configure DI and jwt in program

--- web-api-dot-net/Program.cs
+++ web-api-dot-net/Program.cs
@@ -1,11 +1,30 @@
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using web_api_dot_net.Data.Repositories;
+using web_api_dot_net.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddSingleton<IAuthService, AuthService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options =>
+    {
+        var key = Encoding.ASCII.GetBytes(builder.Configuration["Jwt:Key"]);
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(key),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    })
 
 var app = builder.Build();
 
@@ -18,6 +37,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
