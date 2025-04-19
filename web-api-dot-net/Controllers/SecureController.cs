125708032307861b89f44a211890c7d7386817ce
Daniel Montes <gdanielmontesh9@gmail.com>
2025-04-18 18:50:11 -0600

    feat: add secure controller

---
+++ web-api-dot-net/Controllers/SecureController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace web_api_dot_net.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SecureController : ControllerBase
+    {
+        [HttpGet]
+        [Authorize]
+        public IActionResult GetSecret()
+        {
+            return Ok("You accessed a protected endpoint!");
+        }
+    }
+}
