43f90ac2d98f1152b1a9b4ab49e8ed8538e91825
Daniel Montes <gdanielmontesh9@gmail.com>
2025-04-18 18:42:21 -0600

    feat: add auth controller

---
+++ web-api-dot-net/Controllers/AuthController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using web_api_dot_net.Data.Repositories;
+using web_api_dot_net.Models;
+
+namespace web_api_dot_net.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] User user)
+        {
+            var token = _authService.Authenticate(user.Username, user.Password);
+            if (token == null) return Unauthorized();
+
+            return Ok(new { Token = token });
+        }
+    }
+}
